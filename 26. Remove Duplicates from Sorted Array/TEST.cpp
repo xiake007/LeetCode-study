9986a539d41fe9c6c4e4e654a607006d63abc12a
Wenhe Zhu <zhuwenhe001@163.com>
2020-07-10 08:30:54 +0800

    no message

---
+++ 26. Remove Duplicates from Sorted Array/TEST.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "../../catch2/catch.hpp"
+#include"solution.h"
+
+TEST_CASE("test class", "[factorial]") {
+	Solution s;
+	vector<int> v1{ 1,1,2 };
+	REQUIRE(s.removeDuplicates(v1) == 2);
+	vector<int> v2{0,0,1,1,1,2,2,3,3,4 };
+	REQUIRE(s.removeDuplicates(v2) == 5);
+}
---
+++ 26. Remove Duplicates from Sorted Array/solution.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:    
+	int removeDuplicates(vector<int>& nums) {
+		int idx_pre = 0;
+		for (int idx_next = 1; idx_next < nums.size(); idx_next++) {
+			if (nums[idx_pre] != nums[idx_next]) {
+				idx_pre++;
+				nums[idx_pre] = nums[idx_next];
+			}
+		}
+		return idx_pre + 1;
+	}
+	int removeDuplicates2(vector<int>& nums) {
+		if (nums.size() == 0) return 0;
+		int idx_pre=0;
+		int idx_next=1;
+		int n = nums.size();
+		while (idx_next < n) {
+			int del_m = 0;
+			while (idx_next < n && nums[idx_pre] == nums[idx_next]){
+				idx_next++;
+			}
+			if (idx_next == n) {
+				return idx_pre + 1;
+			}
+			idx_pre++;
+			nums[idx_pre] = nums[idx_next];
+			idx_next++;
+		}
+		return idx_pre+1;
+		
+	}
+};
