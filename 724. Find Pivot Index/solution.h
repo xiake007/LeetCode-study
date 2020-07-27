3055f67ea0c3f10a2ba54d6401fc90557e563d77
Wenhe Zhu <zhuwenhe001@163.com>
2020-07-27 22:34:14 +0800

    no message

--- 724. Find Pivot Index/solution.h
+++ 724. Find Pivot Index/solution.h
@@ -6,6 +6,19 @@ using namespace std;
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        int sum = 0, leftsum = 0;
+        for (auto i : nums) {
+            sum += i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (leftsum == sum - nums[i] - leftsum) {
+                return i;
+            }
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+    int pivotIndex2(vector<int>& nums) {
         int N = nums.size();
         if (N == 0 ) return -1;
         int sum = 0;
