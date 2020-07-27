674f2a0881d01ee6fd2f03953e9e3b2d9dc50cc8
Wenhe Zhu <zhuwenhe001@163.com>
2020-07-28 00:06:57 +0800

    no message

--- 849. Maximize Distance to Closest Person/TEST.cpp
+++ 849. Maximize Distance to Closest Person/TEST.cpp
@@ -7,11 +7,15 @@ TEST_CASE("test class", "[factorial]") {
 	vector<int> v1{ 1,0,0,0,1,0,1 };
 	REQUIRE(s.maxDistToClosest(v1) == 2);
 	vector<int> v2{ 1,0,0,0};
-	REQUIRE(s.maxDistToClosest(v1) == 3);
+	REQUIRE(s.maxDistToClosest(v2) == 3);
 	vector<int> v3{ 1,0};
-	REQUIRE(s.maxDistToClosest(v1) == 1);
-	vector<int> v5{ 1,0,0,1};
-	REQUIRE(s.maxDistToClosest(v1) == 1);
-	vector<int> v6{ 0,0,0,1};
-	REQUIRE(s.maxDistToClosest(v1) == 3);
+	REQUIRE(s.maxDistToClosest(v3) == 1);
+	vector<int> v4{ 0,1};
+	REQUIRE(s.maxDistToClosest(v4) == 1);
+	vector<int> v5{ 0,0,0,1};
+	REQUIRE(s.maxDistToClosest(v5) == 3);
+	vector<int> v6{ 0,0,0,1,0,0,0,0,1};
+	REQUIRE(s.maxDistToClosest(v6) == 3);
+	vector<int> v7{ 1,0,0,0,0,1,0,0,0};
+	REQUIRE(s.maxDistToClosest(v6) == 3);
 }
--- 849. Maximize Distance to Closest Person/solution.h
+++ 849. Maximize Distance to Closest Person/solution.h
@@ -5,7 +5,45 @@ using namespace std;
 
 class Solution {
 public:
+    //利用了最后一个数不为0是，zero会置0
 	int maxDistToClosest(vector<int>& seats) {
+		int dist, zero, i;
 
+		dist = 0;
+		while (seats[dist] == 0)
+			++dist;
+		zero = 0;
+		for (i = dist + 1; i < seats.size(); ++i)
+			if (seats[i] == 0) {
+				++zero;
+			}
+			else {
+				dist = max(dist, (zero + 1) / 2);
+				zero = 0;
+			}
+
+		return max(dist, zero);
+	}
+	int maxDistToClosest2(vector<int>& seats) {
+		int zero_cnt = 0;
+		int max_dist = 0;
+		for (int i = 0; i < seats.size(); i++) {
+			if (seats[i] == 0) {
+				zero_cnt += 1;
+				if (i == seats.size() - 1) {
+					return max(zero_cnt, max_dist);
+				}
+			}
+			else {
+				if (zero_cnt == i) {
+					max_dist = i;
+				}
+				else {
+					max_dist = max((zero_cnt + 1) / 2, max_dist);
+				}
+				zero_cnt = 0;
+			}
+		}
+		return max_dist;
 	}
 };
